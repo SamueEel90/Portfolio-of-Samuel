e951097f6695d16debe6563fa9bfc49f653bb7bd
Samuel <Samkostofik@gmail.com>
2025-06-13 21:09:46 +0200

    fixed image

--- components/About.tsx
+++ components/About.tsx
@@ -3,12 +3,23 @@ import Image from 'next/image';
 const About = () => {
   return (
     <div className="text-secondary text-l mt-25 max-w-xl ml-4 mr-4 md:mr-20 leading-relaxed">
-      Som vedúci oddelenia v spoločnosti <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">Kaufland</strong> s predchádzajúcimi skúsenosťami ako shift leader v McDonald’s.
-      Mojím cieľom je stať sa{' '}
-      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">programátorom</strong> — programovaniu sa venujem denne a neustále sa zlepšujem.
-      Som{' '}
-      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">tímový</strong> hráč, viedol som viacero menších tímov, viem{' '}
-      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">analyticky</strong> myslieť, rychlo sa učím.
+      Som vedúci oddelenia v spoločnosti{' '}
+      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">
+        Kaufland
+      </strong>{' '}
+      s predchádzajúcimi skúsenosťami ako shift leader v McDonald’s. Mojím cieľom je stať sa{' '}
+      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">
+        programátorom
+      </strong>{' '}
+      — programovaniu sa venujem denne a neustále sa zlepšujem. Som{' '}
+      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">
+        tímový
+      </strong>{' '}
+      hráč, viedol som viacero menších tímov, viem{' '}
+      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">
+        analyticky
+      </strong>{' '}
+      myslieť, rychlo sa učím.
       <br />
       Zároveň kladiem veľký dôraz na zdravý a aktívny životný štýl — venujem sa behu, horskej cyklistike a motorsportu.
       <br />
@@ -25,33 +36,35 @@ const About = () => {
       <span className="text-primary">React, React Native, Next.js, Redux</span>
       <br />
       Momentalne Pracujem na projekte:{' '}
-      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">Attend Flow</strong>
-     <div className="flex gap-2 mt-10">
-  <div className="group relative w-1/2 rounded-xl overflow-hidden">
-    <Image
-      src="/images/Profilovka.jpg"
-      alt="Profilovka 1"
-      fill
-      className="w-full h-full object-cover transition duration-500 group-hover:scale-125"
-      sizes="(max-width: 768px) 100vw, 50vw"
-      style={{ objectFit: 'cover' }}
-    />
-    <div className="absolute inset-0 bg-theme-blue/30 backdrop-brightness-90 transition duration-500 group-hover:bg-transparent" />
-  </div>
-
-  <div className="group relative w-1/2 rounded-xl overflow-hidden">
-    <Image
-      src="/images/profilovka2.jpg"
-      alt="Profilovka 2"
-      fill
-      className="w-full h-full object-cover transition duration-500 group-hover:scale-150"
-      sizes="(max-width: 768px) 100vw, 50vw"
-      style={{ objectFit: 'cover' }}
-    />
-    <div className="absolute inset-0 bg-theme-blue/30 backdrop-brightness-90 transition duration-500 group-hover:bg-transparent" />
-  </div>
-</div>
-
+      <strong className="hover:text-theme-blue text-primary cursor-pointer transition-colors duration-200">
+        Attend Flow
+      </strong>
+      <div className="flex gap-2 mt-10">
+        <div className="group w-1/2 rounded-xl overflow-hidden relative">
+          <Image
+            src="/images/Profilovka.jpg"
+            alt="Profilovka 1"
+            width={400}
+            height={400}
+            className="w-full h-full object-cover transition duration-500 group-hover:scale-125"
+            sizes="(max-width: 768px) 100vw, 50vw"
+            style={{ objectFit: 'cover' }}
+          />
+          <div className="absolute inset-0 bg-theme-blue/30 backdrop-brightness-90 transition duration-500 group-hover:bg-transparent" />
+        </div>
+        <div className="group w-1/2 rounded-xl overflow-hidden relative">
+          <Image
+            src="/images/profilovka2.jpg"
+            alt="Profilovka 2"
+            width={400}
+            height={400}
+            className="w-full h-full object-cover transition duration-500 group-hover:scale-150"
+            sizes="(max-width: 768px) 100vw, 50vw"
+            style={{ objectFit: 'cover' }}
+          />
+          <div className="absolute inset-0 bg-theme-blue/30 backdrop-brightness-90 transition duration-500 group-hover:bg-transparent" />
+        </div>
+      </div>
     </div>
   );
 };
